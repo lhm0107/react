6d66e53c9b4fe9877d0d2c1ed875cd68c7e7d926
DESKTOP-N4V80RO\lhm0107 <lhm820107@gmail.com>
2023-03-03 00:04:20 +0900

    todolist 목록 추가

---
+++ todolist/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoList from './component/TodoList/TodoList';
+
+function App() {
+	return (
+		<div>
+			<TodoList></TodoList>
+		</div>
+	);
+}
+
+export default App;
